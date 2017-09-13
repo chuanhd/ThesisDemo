a3005d24e874c6574ad1376a9e63524cf7284b72
ChuanHD <hodanhchuan@gmail.com>
2017-09-13 22:37:49 +0700

    Add simple list first page Add ui-ux page (wip)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UIUIDemoPage } from '../ui_and_ux/uiux'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  items = [
+    'UI and UX',
+    'Layout',
+    'Animation',
+    'Web Services',
+    'Third party and services',
+    'Game',
+    'Multithreading and background thread',
+    'Testing'
+  ];
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+
+    if (item  == this.items[0]) {
+      this.navCtrl.push(UIUIDemoPage);
+    } else if (item == this.items[1]) {
+
+    }
+    
+  }
 
   constructor(public navCtrl: NavController) {
 
---
+++ src/pages/ui_and_ux/uiux.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-uiux-demo',
+  templateUrl: 'uiux.html'
+})
+export class UIUIDemoPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
